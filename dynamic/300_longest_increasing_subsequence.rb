7a9f611103b37ee62a102be3e2f240c4dfcdadc4
Michael Yang <themichaelyang@gmail.com>
2023-11-11 02:49:12 -0500

    add 300, longest increasing subsequence

---
+++ dynamic/300_longest_increasing_subsequence.rb
@@ -0,0 +1,85 @@
+# Given an integer array nums, return the length of the longest strictly increasing subsequence.
+# Follow up: Can you come up with an algorithm that runs in O(n log(n)) time complexity?
+
+# recursive with memoization
+# def length_of_lis(nums)
+#   longest = []
+#   memory = []
+  
+#   0.upto(nums.length - 1).map do |i|
+#     longest_including(nums, i, memory)
+#   end.max { |a, b| a.length <=> b.length }.length
+# end
+
+# def longest_including(nums, index, memory)
+#   return memory[index] if memory[index]
+
+#   longest = [nums[index]]
+
+#   0.upto(index - 1).each do |i|
+#     if nums[index] > nums[i] 
+#       # this list append is what slows it down, if we just want the length this can be faster
+#       # I think instead of keeping the whole list around we can backtrack to recover the longest
+#       longest_with_i = longest_including(nums, i, memory) + [nums[index]]
+
+#       if longest_with_i.length > longest.length
+#         longest = longest_with_i
+#       end
+#     end
+#   end
+
+#   memory[index] = longest
+# end
+
+# tabulation: O(n^2)
+def length_of_lis(nums)
+  # longest_from[i] gives the previous index that longest lis including index i is from
+  longest_from = [nil] * nums.length
+  longest_including = [1] * nums.length
+
+  longest = 1
+  longest_last_index = 0
+
+  0.upto(nums.length - 1).each do |i|
+    0.upto(i - 1).each do |j|
+
+      if nums[i] > nums[j]
+        longest_from_j = longest_including[j] + 1
+        
+        if longest_from_j > longest_including[i]
+          longest_including[i] = longest_from_j
+          longest_from[i] = j
+        end
+      end
+    end
+
+    if longest_including[i] > longest 
+      longest = longest_including[i]
+      longest_last_index = i
+    end
+  end
+
+  longest
+  # backtrack_lis(longest_from, longest_last_index, [])
+end
+
+# to efficiently recover the lis
+def backtrack_lis(longest_from, last_index, lis)
+  if last_index
+    lis.unshift(last_index)
+    
+    backtrack_lis(longest_from, longest_from[last_index], lis)
+  else
+    lis
+  end
+end
+
+require_relative "../test"
+extend TestSuite
+
+assert_equals(length_of_lis([1]), 1)
+assert_equals(length_of_lis([1, 2]), 2)
+assert_equals(length_of_lis([1, 3, 2]), 2)
+assert_equals(length_of_lis([1, 3, 2, 4]), 3)
+assert_equals(length_of_lis([1, 5, 4, 3, 2, 1]), 2)
+assert_equals(length_of_lis((1..200).to_a), 200)
--- dynamic/5_longest_palindomic_substring.rb
+++ dynamic/5_longest_palindomic_substring.rb
@@ -3,8 +3,8 @@
 # Is this due to recursion, or is there an issue with my memoization?
 def longest_palindrome(str)
   memory = {}
-  length, first, last = (0...(str.length)).flat_map do |i| 
-    [longest_around(str, i, i, memory), longest_around(str, i, i + 1, memory)]
+  length, first, last = (0...(str.length)).map do |i| 
+    [longest_around(str, i, i, memory), longest_around(str, i, i + 1, memory)].max
   end.max
   
   str[first..last]
@@ -12,15 +12,99 @@ end
 
 def longest_around(str, first, last, memory)
   memory[first] ||= []
-  memory[first][last] ||= if first < 0 || last >= str.length || str[first] != str[last]
-    [0, 0, 0]
+  if memory[first][last] 
+    memory[first][last] 
   else
-    [
-      [last - first + 1, first, last],
-      longest_around(str, first - 1, last + 1, memory),
-    ].max
+    if first < 0 || last >= str.length || str[first] != str[last]
+      [0, 0, 0]
+    else
+      [
+        [last - first + 1, first, last],
+        longest_around(str, first - 1, last + 1, memory)
+      ].max
+    end
   end
 end
+
+# TODO: try the "expand" solution
+# # This times out on Leetcode?
+# def longest_palindrome(str)
+#   subproblems = Array.new(str.length)
+#   longest = 0..0
+
+#   # "length" is sort of a misnomer, since it's really the difference between first and last
+#   0.upto(str.length - 1).each do |length|
+#     0.upto(str.length - 1 - length).each do |first|
+#       last = first + length
+
+#       subproblems[length] ||= Array.new(str.length - 1 - length)
+
+#       if length == 0
+#         subproblems[length][first] = true
+#       elsif length == 1
+#         subproblems[length][first] = str[first] == str[last]
+#       elsif length - 2 >= 0 && first + 1 < subproblems[length - 1].length
+#         subproblems[length][first] = subproblems[length - 2][first + 1] && str[first] == str[last]
+#       end
+
+#       if subproblems[length][first] && (first..last).size > longest.size
+#         longest = first..last
+#       end
+#     end
+#   end
+
+#   str[longest]
+# end
+
+# Triangular runtime, which is O(n^2)
+# This is too slow, apparently? Maybe the lambda?
+# def longest_palindrome(str)
+#   subproblem_store = Array.new(str.length)
+#   subproblem_store.each.with_index do |_, i|
+#     subproblem_store[i] = Array.new(str.length - i)
+#   end
+  
+#   # subproblem(i, j) = true means that str[i..j] is a palindrome
+#   # (i, j) depends on (i - 1, j + 1)
+#   # (i, i) is always true -> single letter is a palindrome
+#   # (i, i + 1) is only true if two letter palindrome
+
+#   # We use a triangular instead of a square 2d array to save memory. 
+#   # We index the subproblem_store by inverting the `last` index.
+#   # e.g. The final subproblem is (0, str.length - 1), which is indexed at [0][0]
+
+#   longest = 0..0
+
+#   subproblem = lambda do |first, last, value=nil|
+#     # Need to check bounds because of negative array indexing
+#     if first >= 0 && first < str.length && last >= 0 && last < str.length
+#       result = subproblem_store[first][str.length - 1 - last] ||= value
+
+#       if value && (first..last).size > longest.size 
+#         longest = (first..last)
+#       end
+
+#       result
+#     else
+#       false
+#     end
+#   end
+
+#   0.upto(str.length - 1).each do |length|
+#     0.upto(str.length - 1 - length).each do |first|
+#       last = first + length
+
+#       subproblem.(first, first, true)
+#       subproblem.(first, first + 1, true) if str[first] == str[first + 1]
+      
+#       is_palindrome = subproblem.(first + 1, last - 1) && str[first] == str[last]
+#       subproblem.(first, last, is_palindrome)
+#     end
+#   end
+
+#   str[longest]
+# end
+
 
 # This memoize is nice because we can keep a reference to the memory.
 # def memoize(method_name, with: {})
@@ -51,5 +135,7 @@ assert_equals(longest_palindrome('caba'), 'aba')
 assert_equals(longest_palindrome('abac'), 'aba')
 assert_equals(longest_palindrome('abba'), 'abba')
 assert_equals(longest_palindrome('abbaba'), 'abba')
+assert_equals(longest_palindrome('abcdefggg'), 'ggg')
+assert_equals(longest_palindrome('abcdefghgj'), 'ghg')
 assert_equals(longest_palindrome('e' * 1000), 'e' * 1000)
 assert_equals(longest_palindrome('e' * 1001), 'e' * 1001)
