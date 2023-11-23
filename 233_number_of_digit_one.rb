9fe777322bf87f8aba2c50da5a3c94456b11b92b
Michael Yang <themichaelyang@gmail.com>
2023-11-23 00:09:50 -0500

    simplify 233 with loop

--- 233_number_of_digit_one.rb
+++ 233_number_of_digit_one.rb
@@ -18,43 +18,58 @@ def memoize(method_name)
   end
 end
 
-memoize def descend_digits(digits, i, max_prefix, ones)
+memoize def descend_digits(digits, i, prefix_matches_n, ones)
   digit = digits[i]
-
   return ones if digit.nil?
-  
-  total = 0
 
-  # if previous digits match n
-  if max_prefix
+  largest_digit = if prefix_matches_n then digit else 9 end
 
-    # stick close to n
-    if digit == 1
-      total += descend_digits(digits, i + 1, true, ones + 1)
-    else
-      total += descend_digits(digits, i + 1, true, ones)
-    end
-
-    if digit > 1
-      # if can include a 1 when smaller than n, include a 1
-      total += descend_digits(digits, i + 1, false, ones + 1)
-    end
-
-    if digit != 0
-      # go smaller than n, exclude digit and 1 as options.
-      # we ignore 0 because it's been added with max_prefix=true
-      total += [(digit - 1), 1].max * descend_digits(digits, i + 1, false, ones)
-    end
-
-    total
-  else
-    # include a 1
-    total += descend_digits(digits, i + 1, false, ones + 1)
-
-    # don't include a 1
-    total += 9 * descend_digits(digits, i + 1, false, ones) 
+  (0..largest_digit).sum do |next_digit|
+    next_ones = if next_digit == 1 then ones + 1 else ones end
+    next_prefix_matches_n = prefix_matches_n && next_digit == digit
+    
+    descend_digits(digits, i + 1, next_prefix_matches_n, next_ones)
   end
 end
+
+# memoize def descend_digits(digits, i, max_prefix, ones)
+#   digit = digits[i]
+
+#   return ones if digit.nil?
+  
+#   total = 0
+
+#   # if previous digits match n
+#   if max_prefix
+
+#     # stick close to n
+#     if digit == 1
+#       total += descend_digits(digits, i + 1, true, ones + 1)
+#     else
+#       total += descend_digits(digits, i + 1, true, ones)
+#     end
+
+#     if digit > 1
+#       # if can include a 1 when smaller than n, include a 1
+#       total += descend_digits(digits, i + 1, false, ones + 1)
+#     end
+
+#     # As Ishaan notes, it's easier to reason about if you collapse this logic.
+#     if digit != 0
+#       # go smaller than n, exclude digit and 1 as options.
+#       # we ignore 0 because it's been added with max_prefix=true
+#       total += [(digit - 1), 1].max * descend_digits(digits, i + 1, false, ones)
+#     end
+
+#     total
+#   else
+#     # include a 1
+#     total += descend_digits(digits, i + 1, false, ones + 1)
+
+#     # don't include a 1
+#     total += 9 * descend_digits(digits, i + 1, false, ones) 
+#   end
+# end
 
 require_relative './test'
 extend TestSuite
