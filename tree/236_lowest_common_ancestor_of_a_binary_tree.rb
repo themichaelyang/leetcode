469e83aee11377789fe30797e8567ccc00c5e508
Michael Yang <themichaelyang@gmail.com>
2023-12-23 03:22:38 -0500

    add compact recursive solution for 236

--- tree/236_lowest_common_ancestor_of_a_binary_tree.rb
+++ tree/236_lowest_common_ancestor_of_a_binary_tree.rb
@@ -17,69 +17,94 @@ class TreeNode
     end
 end
 
-# @param {TreeNode} root
-# @param {TreeNode} p
-# @param {TreeNode} q
-# @return {TreeNode}
+# recursive, compact solution, inspired by: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/solutions/65225/4-lines-c-java-python-ruby/
 def lowest_common_ancestor(root, p, q)
-  p, p_path = find_node_and_path(root, p)
-  q, q_path = find_node_and_path(root, q)
-
-  lowest_ancestor = root
-  p_path.each_with_index do |node, i|
-    if node == q_path[i]
-      lowest_ancestor = node
-    else
-      break
-    end
-  end
-
-  lowest_ancestor
+  lowest_containing(root, p, q)
 end
 
-def find_node_and_path(root, node)
-  queue = [root]
-  parents = {root => nil}
-  while current = queue.shift()
-    if current == node
-      found = current
-      break
-    else 
-      if current.left
-        queue.push(current.left)
-        parents[current.left] = current
-      end
-
-      if current.right
-        queue.push(current.right)
-        parents[current.right] = current
-      end
-    end
-  end
-
-  if found
-    [found, find_path(found, parents)]
+# lowest containing both, or just one if both are not possible.
+def lowest_containing(node, p, q)
+  if node == p or node == q or node.nil?
+    node 
   else
-    [nil, []]
+    left_lowest = lowest_containing(node.left, p, q)
+    right_lowest = lowest_containing(node.right, p, q)
+
+    # if the left tree contains p or q and the right tree containss p or q, the current node is a common ancestor.
+    if left_lowest and right_lowest
+      node 
+    else
+      # if any is nil, it means that tree does not contain p or q
+      left_lowest || right_lowest 
+    end
   end
 end
 
-def find_path(dest, parents)
-  path = [dest]
-  node = dest
-  while node = parents[node]
-    path.prepend(node)
-  end
+# iterative solution: 
 
-  path
-end
+# # @param {TreeNode} root
+# # @param {TreeNode} p
+# # @param {TreeNode} q
+# # @return {TreeNode}
+# def lowest_common_ancestor(root, p, q)
+#   p, p_path = find_node_and_path(root, p)
+#   q, q_path = find_node_and_path(root, q)
 
-require_relative '../test'
-extend TestSuite
+#   lowest_ancestor = root
+#   p_path.each_with_index do |node, i|
+#     if node == q_path[i]
+#       lowest_ancestor = node
+#     else
+#       break
+#     end
+#   end
 
-c = TreeNode.new(3)
-b = TreeNode.new(2)
-a = TreeNode.new(1)
-a.left = b
-a.right = c
-assert_equals(find_node_and_path(a, c), [c, [a, c]])
+#   lowest_ancestor
+# end
+
+# def find_node_and_path(root, node)
+#   queue = [root]
+#   parents = {root => nil}
+#   while current = queue.shift()
+#     if current == node
+#       found = current
+#       break
+#     else 
+#       if current.left
+#         queue.push(current.left)
+#         parents[current.left] = current
+#       end
+
+#       if current.right
+#         queue.push(current.right)
+#         parents[current.right] = current
+#       end
+#     end
+#   end
+
+#   if found
+#     [found, find_path(found, parents)]
+#   else
+#     [nil, []]
+#   end
+# end
+
+# def find_path(dest, parents)
+#   path = [dest]
+#   node = dest
+#   while node = parents[node]
+#     path.prepend(node)
+#   end
+
+#   path
+# end
+
+# require_relative '../test'
+# extend TestSuite
+
+# c = TreeNode.new(3)
+# b = TreeNode.new(2)
+# a = TreeNode.new(1)
+# a.left = b
+# a.right = c
+# assert_equals(find_node_and_path(a, c), [c, [a, c]])
