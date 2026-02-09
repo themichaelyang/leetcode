1b15e24318b9431d9f57786e6faab853362a9a52
Michael Yang <themichaelyang@gmail.com>
2026-02-09 01:21:49 -0800

    add 238: product of array except self

---
+++ 238_product_of_array_except_self.rb
@@ -0,0 +1,34 @@
+# Given an integer array nums, return an array answer such that answer[i] is equal
+# to the product of all the elements of nums except nums[i].
+
+# The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+# You must write an algorithm that runs in O(n) time and without using the division operation.
+
+# Follow up: Can you solve the problem in O(1) extra space complexity?
+# (The output array does not count as extra space for space complexity analysis.)
+
+# @param {Integer[]} nums
+# @return {Integer[]}
+def product_except_self(nums)
+  prefixes = []
+  suffixes = []
+
+  nums.length.times do |i|
+    prefixes << (prefixes.last || 1) * nums[i]
+    suffixes << (suffixes.last || 1) * nums[nums.length - 1 - i]
+  end
+
+  nums.length.times.map do |i|
+    prefix = i - 1 >= 0 ? prefixes[i - 1] : 1
+    suffix = nums.length - 2 - i >= 0 ? suffixes[nums.length - 2 - i] : 1
+    prefix * suffix
+  end
+end
+
+require_relative './testing'
+Testing.expect(product_except_self([1, 2, 3, 4]), [24, 12, 8, 6])
+Testing.expect(product_except_self([-1, 1, 0, -3, 3]), [0, 0, 9, 0, 0])
+Testing.expect(product_except_self([3, 2]), [2, 3])
+Testing.expect(product_except_self([-1, 1, 1000]), [1000, -1000, -1])
+Testing.summary
