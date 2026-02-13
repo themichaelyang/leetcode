b01cef0de626de23828a0a717b8a6208851ffc18
Michael Yang <themichaelyang@gmail.com>
2026-02-12 16:07:59 -0800

    add 643: maximum average subarray i

---
+++ 643_maximum_average_subarray_i.rb
@@ -0,0 +1,26 @@
+# You are given an integer array nums consisting of n elements, and an integer k.
+
+# Find a contiguous subarray whose length is equal to k that has the maximum
+# average value and return this value.
+
+# Any answer with a calculation error less than 10-5 will be accepted.
+  
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Float}
+def find_max_average(nums, k)
+  window_sum = nums[0...k].sum
+  
+  (k...nums.length).reduce(window_sum / k.to_f) do |max_avg, i|
+    window_sum += nums[i]
+    window_sum -= nums[i - k]
+
+    [max_avg, window_sum / k.to_f].max
+  end
+end
+
+require_relative 'testing'
+Testing.expect(find_max_average([1, 12, -5, -6, 50, 3], 4), 12.75)
+Testing.expect(find_max_average([5], 1), 5)
+Testing.expect(find_max_average([1, 2, 3, 4, 5, 6, 7, 8], 2), 7.5)
+Testing.summary
