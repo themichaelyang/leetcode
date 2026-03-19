3ff1e7f8b5e561cf9057a48a8a26171b2eff26db
Michael Yang <themichaelyang@gmail.com>
2026-03-19 15:18:16 -0700

    add comment

--- 49_group_anagrams.rb
+++ 49_group_anagrams.rb
@@ -1,3 +1,5 @@
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
 # @param {String[]} strs
 # @return {String[][]}
 def group_anagrams_sorting(strs)
