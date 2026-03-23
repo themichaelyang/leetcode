39edb5b2d6abd3d1db68073410180fade46a6d00
Michael Yang <themichaelyang@gmail.com>
2026-03-23 00:40:28 -0700

    add comment

--- 271_encode_and_decode_strings.py
+++ 271_encode_and_decode_strings.py
@@ -1,3 +1,9 @@
+# Design an algorithm to encode a list of strings to a string.
+# The encoded string is then sent over the network and is decoded
+# back to the original list of strings.
+#
+# (neetcode since leetcode was paywalled)
+
 class Solution:
   def encode(self, strs: list[str]) -> str:
     ret = []
