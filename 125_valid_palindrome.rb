8f382c9b5887c2bfb33991405f2d8f6ac226a402
Michael Yang <themichaelyang@gmail.com>
2026-03-23 16:33:32 -0700

    add loop variation of 125: valid palindrome

--- 125_valid_palindrome.rb
+++ 125_valid_palindrome.rb
@@ -5,20 +5,36 @@
 
 # Given a string s, return true if it is a palindrome, or false otherwise.
 
-# @param {String} s
-# @return {Boolean}
-def is_palindrome(s)
+def is_palindrome_reverse(s)
   # /i = case insensitive
   # ^ = negate char group
   str = s.downcase.gsub(/[^0-9a-z]/i, '')
   str == str.reverse
 end
 
+# @param {String} s
+# @return {Boolean}
+def is_palindrome(s)
+  str = s.downcase.gsub(/[^0-9a-z]/i, '')
+  # ruby ranges only go left to right unfort, but compare:
+  # str[0..((str.length - 1) / 2)] == str[-((str.length + 1) / 2)..-1].reverse
+
+  0.upto(str.length / 2).all? do |i|
+    str[i] == str[str.length - 1 - i]
+  end
+end
+
+# 1 = -1
+# 2 = -1
+# 3 = -2
+# 4 = -2
+
 require_relative './testing'
 Testing.assert(is_palindrome('a'))
 Testing.assert(is_palindrome('aa'))
 Testing.assert(is_palindrome('aaa'))
 Testing.assert(is_palindrome('bab'))
+Testing.assert(is_palindrome('baab'))
 Testing.assert(is_palindrome('cbabc'))
 Testing.assert(is_palindrome('cba!b c'))
 Testing.refute(is_palindrome('ab'))
