30275dfa57b3b4e31ab2d76e743b6195fb146075
Michael Yang <themichaelyang@gmail.com>
2026-03-24 16:18:51 -0700

    add 141: linked list cycle

---
+++ 141_linked_list_cycle.rb
@@ -0,0 +1,55 @@
+# Definition for singly-linked list.
+class ListNode
+  attr_accessor :val, :next
+  def initialize(val)
+    @val = val
+    @next = nil
+  end
+
+  def self.from_leetcode(list, cycle_pos=-1)
+    nodes = list.map do |val|
+      ListNode.new(val)
+    end
+
+    nodes.each_cons(2).each do |prev, curr|
+      prev.next = curr
+    end
+
+    nodes.last.next = nodes[cycle_pos] if cycle_pos >= 0
+
+    nodes.first
+  end
+end
+
+# node = ListNode.from_leetcode([3,2,0,-4], 1)
+# loop do
+#   puts node.val
+#   node = node.next
+#   break if node.nil?
+#   sleep 1
+# end
+
+# @param {ListNode} head
+# @return {Boolean}
+# annoyed that this isn't snake_case in leetcode
+# O(1) space with floyds
+def hasCycle(head)
+  false if head.nil?
+  tortoise, hare = [head, head]
+
+  loop do
+    tortoise = tortoise&.next
+    hare = hare&.next&.next
+    break if tortoise == hare
+  end
+
+  !tortoise.nil?
+end
+
+require_relative './testing'
+Testing.assert(hasCycle(ListNode.from_leetcode([3,2,0,-4], 1)))
+Testing.assert(hasCycle(ListNode.from_leetcode([1, 2], 0)))
+Testing.refute(hasCycle(ListNode.from_leetcode([1, 2], -1)))
+Testing.assert(hasCycle(ListNode.from_leetcode([1], 0)))
+Testing.refute(hasCycle(ListNode.from_leetcode([], -1)))
+Testing.summary
