aba0377a36ee77962281bae52a353b2cf7d49955
Michael Yang <themichaelyang@gmail.com>
2026-03-24 04:04:01 -0700

    add O(n) solution for 110: balanced binary tree

--- 110_balanced_binary_tree.rb
+++ 110_balanced_binary_tree.rb
@@ -6,18 +6,40 @@ require_relative './lib/tree_node'
 
 # @param {TreeNode} root
 # @return {Boolean}
-def is_balanced(root)
-  return true if root.nil?
+# O(n log n): O(log n) for height call, called n times (once per node)
+# O(n^2) worst case due to O(n) height call for skewed tree
+# def is_balanced(root)
+#   return true if root.nil?
 
-  (height(root.left) - height(root.right)).abs <= 1 \
-    && is_balanced(root.left) \
-    && is_balanced(root.right)
+#   (height(root.left) - height(root.right)).abs <= 1 \
+#     && is_balanced(root.left) \
+#     && is_balanced(root.right)
+# end
+
+# could add memoization because this is O(n) for a skewed tree, although
+# that slows the leetcode speed down
+# def height(root)
+#   return 0 if root.nil?
+
+#   [height(root.left), height(root.right)].max + 1
+# end
+
+# @param {TreeNode} root
+# @return {Boolean}
+# O(n) single pass
+def is_balanced(root)
+  height_and_balance(root).last
 end
 
-def height(root)
-  return 0 if root.nil?
+def height_and_balance(node)
+  return [0, true] if node.nil?
+  left_height, left_balance = height_and_balance(node.left)
+  right_height, right_balance = height_and_balance(node.right)
 
-  [height(root.left), height(root.right)].max + 1
+  [
+    [left_height, right_height].max + 1,
+    (left_height - right_height).abs <= 1 && left_balance && right_balance
+  ]
 end
 
 require_relative './testing'
