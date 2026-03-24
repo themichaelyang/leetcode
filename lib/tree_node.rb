b8ab90bc51e52bd442fe92bde63522b216576f34
Michael Yang <themichaelyang@gmail.com>
2026-03-24 01:14:47 -0700

    add another tree node test

--- lib/tree_node.rb
+++ lib/tree_node.rb
@@ -101,4 +101,18 @@ Testing.expect(
     TreeNode.new(7, TreeNode.new(2, TreeNode.new(9)))
   )
 )
+Testing.expect(
+  TreeNode.from_leetcode([
+                     1,
+              2,            3,
+          4,    nil,    nil,    5,
+      6,      7,           8,      9,
+  nil, 10,  11, 12,    nil, nil, nil, 14
+  ]),
+  TreeNode.new(
+    1,
+    TreeNode.new(2, TreeNode.new(4, TreeNode.new(6, nil, TreeNode.new(10)), TreeNode.new(7, TreeNode.new(11), TreeNode.new(12)))),
+    TreeNode.new(3, nil, TreeNode.new(5, TreeNode.new(8), TreeNode.new(9, nil, TreeNode.new(14))))
+  )
+)
 Testing.summary
