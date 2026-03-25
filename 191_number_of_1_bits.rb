cd9c93de3e76de16dfff548a12fa4a8edff58a01
Michael Yang <themichaelyang@gmail.com>
2026-03-24 19:14:58 -0700

    add 3 bit lookup table to 191: number of 1 bits

--- 191_number_of_1_bits.rb
+++ 191_number_of_1_bits.rb
@@ -14,6 +14,24 @@ def hamming_weight(n)
   ones
 end
 
+# # @param {Integer} n
+# # @return {Integer}
+# def hamming_weight(n)
+#   ones = 0
+
+#   while n > 0
+#     ones += case n % 8
+#       when 0b000 then 0
+#       when 0b001, 0b010, 0b100 then 1
+#       when 0b011, 0b101, 0b110 then 2
+#       when 0b111 then 3
+#     end
+#     n = n >> 3
+#   end
+
+#   ones
+# end
+
 require_relative './testing'
 Testing.expect(hamming_weight(0), 0)
 Testing.expect(hamming_weight(1), 1)
