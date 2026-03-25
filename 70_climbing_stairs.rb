20c652a93a493130cb21f6744aa083b4050653cc
Michael Yang <themichaelyang@gmail.com>
2026-03-24 23:29:41 -0700

    add recursive solution for 70: climbing stairs

--- 70_climbing_stairs.rb
+++ 70_climbing_stairs.rb
@@ -5,17 +5,24 @@
 
 # @param {Integer} n
 # @return {Integer}
-def climb_stairs(n)
-  ways = Array.new(n + 1)
-  ways[0] = 0
-  ways[1] = 1
-  ways[2] = 2
+# iterative
+# def climb_stairs(n)
+#   ways = Array.new(n + 1)
+#   ways[0] = 0
+#   ways[1] = 1
+#   ways[2] = 2
 
-  3.upto(n) do |i|
-    ways[i] = ways[i - 2] + ways[i - 1]
-  end
+#   3.upto(n) do |i|
+#     ways[i] = ways[i - 2] + ways[i - 1]
+#   end
 
-  ways[n]
+#   ways[n]
+# end
+
+# recursive
+def climb_stairs(n, cache={})
+  return n if n <= 2
+  cache[n] ||= climb_stairs(n - 1, cache) + climb_stairs(n - 2, cache)
 end
 
 require_relative './testing'
