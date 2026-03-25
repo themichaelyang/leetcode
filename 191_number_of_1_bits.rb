98f623a6fd232689e2c1befcdb011fa1f4733dd3
Michael Yang <themichaelyang@gmail.com>
2026-03-24 19:08:48 -0700

    add 191: number of 1 bits

---
+++ 191_number_of_1_bits.rb
@@ -0,0 +1,26 @@
+# Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
+# Follow up: If this function is called many times, how would you optimize it?
+
+# @param {Integer} n
+# @return {Integer}
+def hamming_weight(n)
+  ones = 0
+
+  while n > 0
+    ones += 1 if n % 2 == 1
+    n = n >> 1
+  end
+
+  ones
+end
+
+require_relative './testing'
+Testing.expect(hamming_weight(0), 0)
+Testing.expect(hamming_weight(1), 1)
+Testing.expect(hamming_weight(2), 1)
+Testing.expect(hamming_weight(3), 2)
+Testing.expect(hamming_weight(4), 1)
+Testing.expect(hamming_weight(0b1010101), 4)
+Testing.expect(hamming_weight(128), 1)
+Testing.expect(hamming_weight(2147483645), 30)
+Testing.summary
