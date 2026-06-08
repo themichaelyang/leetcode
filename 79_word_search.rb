c32270eb222e573ce9ba81c71d39650e9b8b92c7
Michael Yang <themichaelyang@gmail.com>
2026-06-08 00:37:37 -0700

    fix 79: word search by converting to recursive dfs with backtracking

--- 79_word_search.rb
+++ 79_word_search.rb
@@ -5,38 +5,39 @@
 # where adjacent cells are horizontally or vertically neighboring.
 # The same letter cell may not be used more than once.
 
-# "Character" comes from the leetcode template (should prob be String)
 # @param {Character[][]} board
 # @param {String} word
 # @return {Boolean}
 def exist(board, word)
-  height = board.length
-  width = board.first.length
+  height, width = [board.length, board.first.length]
   visited = Array.new(height) { Array.new(width) { false } }
 
-  height.times do |y|
-    width.times do |x|
-      stack = [[[y, x], 0]]
-
-      until stack.empty?
-        board_pos, char_at = stack.pop
-        board_char = board.dig(*board_pos)
-        visited[board_pos.first][board_pos.last] = true
-
-        if word[char_at] == board_char
-          return true if char_at == word.length - 1
-
-          neighbors = list_neighbors(board_pos, width, height)
-          candidates = neighbors.reject { |pos| visited.dig(*pos) }
-          candidates.each { |c| stack.push([c, char_at + 1]) }
-        else
-          visited[board_pos.first][board_pos.last] = false
-        end
-      end
+  height.times.any? do |y|
+    width.times.any? do |x|
+      dfs([y, x], 0, board, visited, width, height, word)
     end
   end
+end
 
-  false
+# recursive backtracking template much easier than iterative to undo visit
+# this is because it tracks tree structure better. with iterative dfs with
+# stack can push an undo sentinel to the stack to unvisit a branch.
+def dfs(pos, char_at, board, visited, width, height, word)
+  return word[char_at] == board.dig(*pos) if char_at == word.length - 1
+  return false if word[char_at] != board.dig(*pos)
+
+  visited[pos.first][pos.last] = true
+
+  available = list_neighbors(pos, width, height)
+    .reject { |nb| visited.dig(*nb) }
+
+  result = available.any? do |av|
+    dfs(av, char_at + 1, board, visited, width, height, word)
+  end
+
+  visited[pos.first][pos.last] = false
+
+  result
 end
 
 def list_neighbors(pos, width, height)
@@ -88,4 +89,8 @@ Testing.refute(exist([["a", "b"], ["c", "d"]], "cb"))
 Testing.refute(exist([["a", "b"], ["c", "d"]], "abcde"))
 Testing.refute(exist([["a", "b"], ["c", "d"]], "abce"))
 
+Testing.assert(exist([["a", "a", "a"], ["b", "b", "a"], ["a", "a", "a"]], "aaaaaaa"))
+Testing.assert(exist([["a", "a", "a"], ["b", "b", "a"], ["a", "a", "a"]], "abaab"))
+
+Testing.assert(exist([["C", "A", "A"], ["A", "A", "A"], ["B", "C", "D"]], "AAB"))
 Testing.summary
